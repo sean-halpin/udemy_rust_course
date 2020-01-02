12376c8dbd6e989d1adf8d6bec507cc198436ec5
Sean <pulpfictionost@gmail.com>
2020-01-02 16:31:36 +0000

    Enums

--- data_structures/src/main.rs
+++ data_structures/src/main.rs
@@ -19,6 +19,44 @@ fn structures() {
     let this_line = Line { start: p, end: p2 };
 }
 
+enum Colour {
+    Red,
+    Green,
+    Blue,
+    RgbColor(u8, u8, u8), // tuple
+    Cmyk {
+        cyan: u8,
+        magenta: u8,
+        yellow: u8,
+        black: u8,
+    }, // struct
+}
+
+fn enums() {
+    // let c:Colour = Colour::Red;
+    let c: Colour = Colour::Cmyk {
+        cyan: 0,
+        magenta: 128,
+        yellow: 0,
+        black: 255,
+    };
+    match c {
+        Colour::Red => println!("r"),
+        Colour::Green => println!("g"),
+        Colour::Blue => println!("b"),
+        Colour::RgbColor(0, 0, 0) => println!("black"),
+        Colour::Cmyk {
+            cyan: _,
+            magenta: _,
+            yellow: _,
+            black: 255,
+        } => println!("black"),
+        Colour::RgbColor(r, g, b) => println!("{},{},{}", r, g, b),
+        _ => ()
+    }
+}
+
 fn main() {
     structures();
+    enums();
 }
