5ad6860ded18a55cdec7ac2eebce811a3eafcdd5
Sean <pulpfictionost@gmail.com>
2020-01-02 17:31:13 +0000

    Slices

--- data_structures/src/main.rs
+++ data_structures/src/main.rs
@@ -170,6 +170,17 @@ fn vectors() {
     }
 }
 
+fn use_slice(slice: &mut [i32]) {
+    println!("first elem {}, len = {}", slice[0], slice.len());
+    slice[0] = 4321;
+}
+
+fn slices() {
+    let mut data = [1, 2, 3, 4, 5];
+    use_slice(&mut data[1..4]);
+    println!("{:?}", data);
+}
+
 fn main() {
     structures();
     enums();
@@ -177,4 +188,5 @@ fn main() {
     options();
     arrays();
     vectors();
+    slices();
 }
