a68172fcdabc305cc386de2d76c5562b6b8850a2
Sean <pulpfictionost@gmail.com>
2020-01-02 23:11:10 +0000

    Drop

---
+++ traits/src/drop_example.rs
@@ -0,0 +1,29 @@
+// equivalent to destructor
+
+struct Creature {
+    name: String,
+}
+
+impl Creature {
+    fn new(name: &str) -> Creature {
+        println!("{} enters the game", name);
+        Creature { name: name.into() }
+    }
+}
+
+impl Drop for Creature {
+    fn drop(&mut self) {
+        println!("{} is dead", self.name)
+    }
+}
+
+pub fn drop_example() {
+    let mut clever: Creature;
+    {
+        let goblin = Creature::new("Jeff");
+        println!("game proceeds");
+        clever = goblin;
+        println!("end of scope");
+        // drop(goblin);
+    }
+}
--- traits/src/main.rs
+++ traits/src/main.rs
@@ -1,7 +1,9 @@
 mod traits;
 mod into_example;
+mod drop_example;
 
 fn main() {
     traits::traits();
     into_example::into_example();
+    drop_example::drop_example();
 }
