12fd4626354061485b150960e558b5296061cf36
Sean Halpin <sean.halpin@jet.com>
2020-01-03 12:59:36 +0000

    Dynamic Dispatch 2

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -1,8 +1,9 @@
-mod traits;
-mod into_example;
 mod drop_example;
+mod dynamic_dispatch;
+mod into_example;
 mod op_overloading;
 mod static_dispatch;
+mod traits;
 
 fn main() {
     traits::traits();
@@ -10,4 +11,5 @@ fn main() {
     drop_example::drop_example();
     op_overloading::op_overloading();
     static_dispatch::static_dispatch();
+    dynamic_dispatch::dynamic_dispatch();
 }
