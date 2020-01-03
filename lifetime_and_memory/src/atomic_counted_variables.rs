cd77c0d7916533838ab7acb851a41c6de37d995f
Sean Halpin <sean.halpin@jet.com>
2020-01-03 16:42:11 +0000

    Mutex for Thread Safe Mutability

--- lifetime_and_memory/src/atomic_counted_variables.rs
+++ lifetime_and_memory/src/atomic_counted_variables.rs
@@ -1,26 +1,39 @@
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use std::thread;
 
 struct Person {
     name: Arc<String>,
+    state: Arc<Mutex<String>>,
 }
 
 impl Person {
-    fn new(name: Arc<String>) -> Person {
-        Person { name: name }
+    fn new(name: Arc<String>, state: Arc<Mutex<String>>) -> Person {
+        Person {
+            name: name,
+            state: state,
+        }
     }
     fn greet(&self) {
+        let mut state = self.state.lock().unwrap();
+        state.clear();
+        state.push_str("excited");
+
         println!("Hi, my name is {}", self.name);
     }
 }
 
 pub fn atomic_counted_variables() {
     let name = Arc::new("John".to_string());
-    let person = Person::new(name.clone());
+    let state = Arc::new(Mutex::new("bored".to_string()));
+    let person = Person::new(name.clone(), state.clone());
 
     let t = thread::spawn(move || {
         person.greet();
     });
-    println!("Name = {}", name);
+    println!(
+        "Name = {}, State = {}",
+        name,
+        state.lock().unwrap().as_str()
+    );
     t.join().unwrap();
 }
