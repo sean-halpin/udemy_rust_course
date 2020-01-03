15f6168694e529a23b51c466286133c586033725
Sean Halpin <sean.halpin@jet.com>
2020-01-03 12:11:12 +0000

    Static Dispatch

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -2,10 +2,12 @@ mod traits;
 mod into_example;
 mod drop_example;
 mod op_overloading;
+mod static_dispatch;
 
 fn main() {
     traits::traits();
     into_example::into_example();
     drop_example::drop_example();
     op_overloading::op_overloading();
+    static_dispatch::static_dispatch();
 }
---
+++ traits/src/static_dispatch.rs
@@ -0,0 +1,37 @@
+trait Printable
+{
+    fn format(&self) -> String;
+}
+
+impl Printable for i32
+{
+    fn format(&self) -> String
+    {
+        format!("i32 {}", *self)
+    }
+}
+
+impl Printable for String
+{
+    fn format(&self) -> String
+    {
+        format!("String {}", *self)
+    }
+}
+
+fn print(z:&Printable){
+
+}
+
+fn print_it<T: Printable>(z: T){
+    println!("{}", z.format());
+} // monomorphisation
+
+pub fn static_dispatch(){
+    let a = 123;
+    let b = "hello".to_string();
+    println!("{}", a.format());
+    println!("{}", b.format());
+    print_it(a);
+    print_it(b);
+}
