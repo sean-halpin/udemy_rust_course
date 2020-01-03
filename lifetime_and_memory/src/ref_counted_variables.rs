fc61339b8545bc212ea20ea022dc1724521de898
Sean Halpin <sean.halpin@jet.com>
2020-01-03 14:31:54 +0000

    Ref Counted Variables

--- lifetime_and_memory/src/ref_counted_variables.rs
+++ lifetime_and_memory/src/ref_counted_variables.rs
@@ -1,3 +1,25 @@
-pub fn ref_counted_variables(){
-    
+use std::rc::Rc;
+
+struct Person {
+    name: Rc<String>,
+}
+
+impl Person {
+    fn new(name: Rc<String>) -> Person {
+        Person { name: name }
+    }
+    fn greet(&self) {
+        println!("Hi, my name is {}", self.name);
+    }
+}
+
+pub fn ref_counted_variables() {
+    let name = Rc::new("John".to_string());
+    println!("Name = {}, Ref Count = {}", name, Rc::strong_count(&name));
+    {
+        let person = Person::new(name.clone());
+        println!("Name = {}, Ref Count = {}", name, Rc::strong_count(&name));
+        person.greet();
+    }
+    println!("Name = {}, Ref Count = {}", name, Rc::strong_count(&name));
 }
